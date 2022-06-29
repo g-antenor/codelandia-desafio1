b7bde4bd98bf3083e7dd1b0193037610038985d1
Guilherme Antenor <84650076+g-antenor@users.noreply.github.com>
2022-06-29 14:50:28 -0400

    Add files via upload

---
+++ script/animation.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('.card', { delay: 100 });
+ScrollReveal().reveal('date', { delay: 200 });
+ScrollReveal().reveal('.link-title', { delay: 300 });
+ScrollReveal().reveal('.description', { delay: 300 });
